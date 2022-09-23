5595571436b0ab0683a4728a1f27b8cf5a5f7bdf
Gpalacios <gpobregon1@gmail.com>
2022-09-22 19:52:36 -0600

    [UPDATED] test drag & drop target

--- src/app/modules/Test/index.jsx
+++ src/app/modules/Test/index.jsx
@@ -1,14 +1,24 @@
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import DragDrop from "./components/DragDrop";
-const Index = () => {
-    return (
-        <DndProvider backend={HTML5Backend}>
-            <div className="App">
-                <DragDrop />
-            </div>
-        </DndProvider>
-    )
-}
+import { Box } from './Box.js'
+import { Dustbin } from './Dustbin.js'
+export const Container = () => (
+  <div>
+    <div style={{ overflow: 'hidden', clear: 'both', margin: '-1rem' }}>
+      <Dustbin greedy={true}>
+        <Dustbin greedy={true}>
+          <Dustbin greedy={true} />
+        </Dustbin>
+      </Dustbin>
+      <Dustbin>
+        <Dustbin>
+          <Dustbin />
+        </Dustbin>
+      </Dustbin>
+    </div>
 
-export default Index
+    <div style={{ overflow: 'hidden', clear: 'both', marginTop: '1.5rem' }}>
+      <Box />
+    </div>
+  </div>
+)
+
+export default Container
