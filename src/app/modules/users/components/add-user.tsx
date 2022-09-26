bf50357fc474e45cb5e4a63084d78094ddb72317
Osmar Ortíz <osmar.ortiz@innappgt.com>
2022-09-26 13:57:28 -0600

    🐛 Bugfix changed alerts in user management page

--- src/app/modules/users/components/add-user.tsx
+++ src/app/modules/users/components/add-user.tsx
@@ -1,7 +1,7 @@
-import React, { useState, FC, useEffect } from 'react'
+import React, {useState, FC, useEffect} from 'react'
 import Select from 'react-select'
 import makeAnimated from 'react-select/animated'
-import { Button, Modal, Form, Row, Col } from 'react-bootstrap'
+import {Button, Modal, Form, Row, Col} from 'react-bootstrap'
 import {
     validateStringEmail,
     validateStringEmailAlert,
@@ -11,16 +11,11 @@ import {
     validateStringPhoneNumberAlert,
     validateStringSinCaracteresEspeciales,
 } from '../../validarCadena/validadorCadena'
-import { roleManager } from '../../../models/roleManager'
-import {
-    addUserMethod,
-    getData,
-    getRolesMethod,
-    postData, 
-} from '../../../services/api'
-import { Amplify, Auth } from 'aws-amplify'
-import { awsconfig } from '../../../../aws-exports'
-import { useAuth } from '../../auth/core/Auth'
+import {roleManager} from '../../../models/roleManager'
+import {addUserMethod, getData, getRolesMethod, postData} from '../../../services/api'
+import {Amplify, Auth} from 'aws-amplify'
+import {awsconfig} from '../../../../aws-exports'
+import {useAuth} from '../../auth/core/Auth'
 import swal from 'sweetalert'
 
 Amplify.configure(awsconfig)
@@ -78,42 +73,14 @@ const customStyles = {
 
 const animatedComponents = makeAnimated()
 
-
-
-const options = [
-    {value: 'Admnistrador', label: 'Administrador'},
-    {value: 'Editor', label: 'Editor'},
-    {value: 'Gestor', label: 'Gestor'},
-]
-
-const alertLlenar = async () => {
-    swal({
-        text: '¡campos incompletos!',
-        icon: 'warning',
-    })
-}
-
 const alertEmail = async () => {
     swal({
         text: '¡Email invalido!',
         icon: 'warning',
     })
 }
-const alertEmailNoIngresado = async () => {
-    swal({
-        text: '¡Email no ingresado!',
-        icon: 'warning',
-    })
-}
 
-const campos = async () => {
-    swal({
-        text: 'ha ocurrido un error, verifica si ingresaste un email no registrado',
-        icon: 'warning',
-    })
-}
-
-const AddUser: FC<any> = ({ show, onClose }) => {
+const AddUser: FC<any> = ({show, onClose}) => {
     const [user, setUser] = useState({
         username: '',
         password: '',
@@ -122,20 +89,29 @@ const AddUser: FC<any> = ({ show, onClose }) => {
         role: '',
         passwordConfirm: '',
         phoneNumber: '',
-        imageProfile: 'https://mcd-backoffice-upload.s3.us-east-2.amazonaws.com/fotoPerfiles/Usuario-Vacio-300x300.png'
+        imageProfile:
+            'https://mcd-backoffice-upload.s3.us-east-2.amazonaws.com/fotoPerfiles/Usuario-Vacio-300x300.png',
     })
 
+    const [validEmail, setValidEmail] = useState(false)
+    const [touchedEmailInput, setTouchedEmailInput] = useState(false)
 
+    const [validPassword, setValidPassword] = useState(false)
+    const [touchedPasswordInput, setTouchedPasswordInput] = useState(false)
 
+    const [validRepeatPassword, setValidRepeatPassword] = useState(false)
+    const [touchedRepeatPasswordInput, setTouchedRepeatPasswordInput] = useState(false)
+
+    const [validPhone, setValidPhone] = useState(false)
+    const [touchedPhoneInput, setTouchedPhoneInput] = useState(false)
 
     const [roles, setRoles] = useState<roleManager[]>([])
-    console.log("roles: ", roles);
     //TODO: get roles
     const getRoles = async () => {
         const role: any = await getData(getRolesMethod)
         setRoles(role.data as roleManager[])
     }
-    // console.log(getRoles())   
+    // console.log(getRoles())
 
     useEffect(() => {
         getRoles()
@@ -143,21 +119,32 @@ const AddUser: FC<any> = ({ show, onClose }) => {
 
     const rolesOptions = roles.map((role) => ({
         value: role.nombre,
-        label: role.nombre
+        label: role.nombre,
     }))
 
-
-    // for (let rol of roles) {
-    //     console.log(rol)
-    // }
-    // console.log('----------------------------------------')
-
     const signUp = async () => {
-        if (user.lastname != '' && user.name != '' && user.password != '' && user.passwordConfirm != '' && user.role != '' && user.username != '' && user.phoneNumber != '') {
+        if (
+            user.lastname != '' &&
+            user.name != '' &&
+            user.password != '' &&
+            user.passwordConfirm != '' &&
+            user.role != '' &&
+            user.username != '' &&
+            user.phoneNumber != ''
+        ) {
+            const regExPassword =
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/g
+            if (regExPassword.test(user.password) == false) {
+                swal(
+                    'La contraseña no es válida',
+                    'La contraseña debe de contener:\n• Al menos una letra mayúscula, una minúscula, un número y un caracter especial (@$!%*?&)\n• Tener al menos un total de 8 caracteres\n',
+                    'warning'
+                )
+                return
+            }
             if (user.password == user.passwordConfirm) {
-
                 const regEx = /[a-zA-Z0-9._%+-]+@[a-z0-9·-]+\.[a-z]{2,8}(.[a-z{2,8}])?/g
-                const regExPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/g
+
                 if (regEx.test(user.username)) {
                     try {
                         const userData = await Auth.signUp({
@@ -175,35 +162,45 @@ const AddUser: FC<any> = ({ show, onClose }) => {
                                 enabled: false,
                             },
                         })
-                        console.log("user: ", user);
-                        const filter = roles.filter((item) => { return user.role === item.nombre })
-                        let objeto = { id_usuario: userData.userSub, id_rol: filter[0].id_rol, foto: user.imageProfile }
-                        await postData(addUserMethod, objeto).then(data => { console.log(data) })
+                        console.log('user: ', user)
+                        const filter = roles.filter((item) => {
+                            return user.role === item.nombre
+                        })
+                        let objeto = {
+                            id_usuario: userData.userSub,
+                            id_rol: filter[0].id_rol,
+                            foto: user.imageProfile,
+                        }
+                        await postData(addUserMethod, objeto).then((data) => {
+                            console.log(data)
+                        })
 
                         // alertUserDone()
                         onClose()
                         document.location.href = '/usuarios/user-management'
                     } catch (error) {
-
                         console.log('error signing up:', error)
-                        swal('Contraseña o email invalidos', 'Recuerda escribir una contraseña que incluya un signo especial, una letra minuscula, una letra mayuscula y un minimo de 6 caracteres en total', 'warning')
-                        return false;
+                        swal(
+                            'Contraseña o email invalidos',
+                            'Recuerda escribir una contraseña que incluya un signo especial, una letra minuscula, una letra mayuscula y un minimo de 6 caracteres en total',
+                            'warning'
+                        )
+                        return false
                     }
                 } else if (!regEx.test(user.username) && user.username !== '') {
                     alertEmail()
                 }
             } else {
-                swal('Las contraseñas no son iguales', 'Intentalo de nuevo', 'warning')
+                swal('Las contraseñas no coninciden', 'Intentalo de nuevo', 'warning')
             }
         } else {
             swal('Campos inválidos', 'Por favor ingresa correctamente los campos', 'warning')
         }
-        //Registro del usuario de AWS en base de datos de back office  
-        //paso 1: recuperar el id proveniente de AWS  
+        //Registro del usuario de AWS en base de datos de back office
+        //paso 1: recuperar el id proveniente de AWS
 
-        //paso 2: recuperar el id del role de back office 
-        //paso 3: consumir la api de usuario guardando los valores del paso 1 y 2 y la imagen dejarla vacia  
-
+        //paso 2: recuperar el id del role de back office
+        //paso 3: consumir la api de usuario guardando los valores del paso 1 y 2 y la imagen dejarla vacia
     }
 
     const handleChangeRole = (event: any) => {
@@ -218,8 +215,6 @@ const AddUser: FC<any> = ({ show, onClose }) => {
             imageProfile: user.imageProfile,
         })
     }
-
-    // console.log(user)
 
     return (
         <>
@@ -249,7 +244,7 @@ const AddUser: FC<any> = ({ show, onClose }) => {
                                             })
                                         }
                                     }}
-                                ></Form.Control>
+                                />
                             </Form.Group>
                         </Col>
 
@@ -273,7 +268,7 @@ const AddUser: FC<any> = ({ show, onClose }) => {
                                             })
                                         }
                                     }}
-                                ></Form.Control>
+                                />
                             </Form.Group>
                         </Col>
 
@@ -284,6 +279,8 @@ const AddUser: FC<any> = ({ show, onClose }) => {
                                     type='text'
                                     className='mb-4'
                                     onChange={(e) => {
+                                        setTouchedEmailInput(true)
+
                                         if (validateStringEmail(e.target.value)) {
                                             setUser({
                                                 username: e.target.value,
@@ -296,11 +293,19 @@ const AddUser: FC<any> = ({ show, onClose }) => {
                                                 imageProfile: user.imageProfile,
                                             })
                                         }
+
+                                        setValidEmail(validateStringEmail(e.target.value))
                                     }}
-                                    onBlur={(e) => {
-                                        validateStringEmailAlert(e.target.value)
-                                    }}
-                                ></Form.Control>
+                                />
+                                {validEmail && touchedEmailInput ? (
+                                    <Form.Label className='text-primary'>
+                                        Correo electrónico válido
+                                    </Form.Label>
+                                ) : !validEmail && touchedEmailInput ? (
+                                    <Form.Label className='text-danger'>
+                                        Correo electrónico no válido
+                                    </Form.Label>
+                                ) : null}
                             </Form.Group>
                         </Col>
 
@@ -325,8 +330,9 @@ const AddUser: FC<any> = ({ show, onClose }) => {
                                     type='number'
                                     autoComplete='off'
                                     className='mb-4'
-                                    maxLength={8}
                                     onChange={(e) => {
+                                        setTouchedPhoneInput(true)
+
                                         if (validateStringPhoneNumber(e.target.value)) {
                                             setUser({
                                                 username: user.username,
@@ -339,11 +345,18 @@ const AddUser: FC<any> = ({ show, onClose }) => {
                                                 imageProfile: user.imageProfile,
                                             })
                                         }
+                                        setValidPhone(validateStringPhoneNumber(e.target.value))
                                     }}
-                                    onBlur={(e) => {
-                                        validateStringPhoneNumberAlert(e.target.value)
-                                    }}
-                                ></Form.Control>
+                                />
+                                {validPhone && touchedPhoneInput ? (
+                                    <Form.Label className='text-primary'>
+                                        Teléfono válido
+                                    </Form.Label>
+                                ) : !validPhone && touchedPhoneInput ? (
+                                    <Form.Label className='text-danger'>
+                                        Teléfono no válido
+                                    </Form.Label>
+                                ) : null}
                             </Form.Group>
                         </Col>
 
@@ -354,23 +367,31 @@ const AddUser: FC<any> = ({ show, onClose }) => {
                                     type='password'
                                     className={'mb-4'}
                                     onChange={(e) => {
-                                        if (validateStringPassword(e.target.value)) {
-                                            setUser({
-                                                username: user.username,
-                                                password: e.target.value,
-                                                name: user.name,
-                                                lastname: user.lastname,
-                                                role: user.role,
-                                                passwordConfirm: user.passwordConfirm,
-                                                phoneNumber: user.phoneNumber,
-                                                imageProfile: user.imageProfile,
-                                            })
-                                        }
+                                        setTouchedPasswordInput(true)
+
+                                        setUser({
+                                            username: user.username,
+                                            password: e.target.value,
+                                            name: user.name,
+                                            lastname: user.lastname,
+                                            role: user.role,
+                                            passwordConfirm: user.passwordConfirm,
+                                            phoneNumber: user.phoneNumber,
+                                            imageProfile: user.imageProfile,
+                                        })
+
+                                        setValidPassword(validateStringPassword(e.target.value))
                                     }}
-                                    onBlur={(e) => {
-                                        validateStringPasswordAlert(e.target.value)
-                                    }}
-                                ></Form.Control>
+                                />
+                                {validPassword && touchedPasswordInput ? (
+                                    <Form.Label className='text-primary'>
+                                        Contraseña válida
+                                    </Form.Label>
+                                ) : !validPassword && touchedPasswordInput ? (
+                                    <Form.Label className='text-danger'>
+                                        Contraseña no válida
+                                    </Form.Label>
+                                ) : null}
                             </Form.Group>
                         </Col>
 
@@ -381,23 +402,33 @@ const AddUser: FC<any> = ({ show, onClose }) => {
                                     type='password'
                                     className={'mb-4'}
                                     onChange={(e) => {
-                                        if (validateStringPassword(e.target.value)) {
-                                            setUser({
-                                                username: user.username,
-                                                password: user.password,
-                                                name: user.name,
-                                                lastname: user.lastname,
-                                                role: user.role,
-                                                passwordConfirm: e.target.value,
-                                                phoneNumber: user.phoneNumber,
-                                                imageProfile: user.imageProfile,
-                                            })
-                                        }
+                                        setTouchedRepeatPasswordInput(true)
+
+                                        setUser({
+                                            username: user.username,
+                                            password: user.password,
+                                            name: user.name,
+                                            lastname: user.lastname,
+                                            role: user.role,
+                                            passwordConfirm: e.target.value,
+                                            phoneNumber: user.phoneNumber,
+                                            imageProfile: user.imageProfile,
+                                        })
+
+                                        setValidRepeatPassword(
+                                            validateStringPassword(e.target.value)
+                                        )
                                     }}
-                                    onBlur={(e) => {
-                                        validateStringPasswordAlert(e.target.value)
-                                    }}
-                                ></Form.Control>
+                                />
+                                {user.password == user.passwordConfirm ? (
+                                    <Form.Label className='text-primary'>
+                                        Las contraseñas coinciden
+                                    </Form.Label>
+                                ) : user.password != user.passwordConfirm ? (
+                                    <Form.Label className='text-danger'>
+                                        Las contraseñas no coinciden
+                                    </Form.Label>
+                                ) : null}
                             </Form.Group>
                         </Col>
                     </Row>
