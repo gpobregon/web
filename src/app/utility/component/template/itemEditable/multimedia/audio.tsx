a5dfb016d79eac83f2ffba7599bf4568e24aecc7
Gpalacios <gpobregon1@gmail.com>
2022-10-10 11:05:37 -0600

    [UPDATED] filtro de busqueda en elemento de recursos y multimedia

--- src/app/utility/component/template/itemEditable/multimedia/audio.tsx
+++ src/app/utility/component/template/itemEditable/multimedia/audio.tsx
@@ -1,17 +1,26 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { FC } from "react";
+import { FC, useContext, useState } from "react";
+import Masonry from 'react-masonry-css'
+import Videos from '../../../resource/video'
+import PerfectScrollbar from 'react-perfect-scrollbar'
+import { Popover, OverlayTrigger, Button } from 'react-bootstrap'
 import { Menu, Item, useContextMenu } from "react-contexify";
+import { ContentContext } from '../../../../../modules/template/movil/context'
 
 type Model = {
   data: any
   referencia: any
   handlerId: any
-  isDragging: any
   setEditItem: (data: any) => void
   updateElement: (data: any) => void
   removeItem: (data: any) => void
 }
-const Audio: FC<Model> = ({ isDragging, referencia, handlerId, data, setEditItem, updateElement, removeItem }) => {
+const Audio: FC<Model> = ({ referencia, handlerId, data, setEditItem, updateElement, removeItem }) => {
+
+  const { allResources, destroyOneResource, changeTypeEdit } = useContext(ContentContext)
+  const [selected, setSelected] = useState<any>([])
+
+  const breakpointColumnsObj = { default: 2, 1100: 2, 700: 2, 500: 2 }
 
   const idMenu = `menu-${data.id}`
 
@@ -21,7 +30,54 @@ const Audio: FC<Model> = ({ isDragging, referencia, handlerId, data, setEditItem
     removeItem(e.triggerEvent.target.id);
     setEditItem([])
   }
-  
+
+  const changeText = (e: any) => {
+    const edit = {
+      ...data,
+      ...e
+    }
+    updateElement(edit)
+  }
+
+  const removeImage = () => {
+    changeText({ url: '' })
+    setSelected([])
+  }
+
+  const Listo = () => {
+    selected.url && changeText({ url: selected.url })
+  }
+
+  const popoverClick = (
+    <Popover id="popover-basic" style={{ transform: 'translate(-366px, 317px)', width: '358px' }}>
+      <Popover.Header as="h3">Videos</Popover.Header>
+      <Popover.Body>
+        <PerfectScrollbar style={{ height: '250px', maxWidth: '400px', width: '100%' }} className="min-tumnail px-4">
+          <Masonry breakpointCols={breakpointColumnsObj} className="my-masonry-grid" columnClassName="my-masonry-grid_column">
+            {
+              allResources.map((file: any, index: number) => {
+                return (
+                  file.tipo.includes('audio/') && <Videos key={index} item={file} selected={selected} setSelected={setSelected} />
+                )
+              })
+            }
+          </Masonry>
+        </PerfectScrollbar>
+      </Popover.Body>
+      <hr />
+      <Popover.Header>
+        <div className="d-flex">
+          <div className="flex-shrink-1 px-4 d-flex justify-content-center align-items-center">
+            <i className="bi bi-trash text-danger fs-2 cursor-pointer" onClick={() => removeImage()} />
+          </div>
+          <div className="w-100 d-grid gap-2">
+            <Button size="sm" onClick={() => Listo()}>Listo</Button>
+          </div>
+        </div>
+      </Popover.Header>
+    </Popover>
+  );
+
   return (
     <div
       ref={referencia}
@@ -37,25 +93,60 @@ const Audio: FC<Model> = ({ isDragging, referencia, handlerId, data, setEditItem
       </div>
       <Menu id={idMenu} theme="dark" data-test={data}>
         <Item onClick={(e: any) => destroyItem(e)}>
-          <div>
-            <i className="bi bi-x-circle-fill text-danger pe-4" />Quitar Elemento
-          </div>
+          <i className="bi bi-x-circle-fill text-danger pe-4" />Quitar Elemento
         </Item>
       </Menu>
-      <div className={`editable ${data.textAling} w-100`}>
+      <div id={data.id} className={`editable d-flex justify-content-center ${data.textAling} w-100`}>
         {
-          data.url ?
-            (<audio controls>
+          !data.url && changeTypeEdit === 1 &&
+          (
+            <audio controls>
+              Su navegador no es compatible con el elemento de audio.
+            </audio>
+          )
+        }
+
+        {
+          data.url && changeTypeEdit === 1 && (
+            <audio controls>
               <source src={data.url} type="audio/ogg" />
               <source src={data.url} type="audio/mpeg" />
               <source src={data.url} type="audio/mp3" />
               Su navegador no es compatible con el elemento de audio.
-            </audio>)
-            :
-            (<audio controls>
-              Su navegador no es compatible con el elemento de audio.
-            </audio>)
+            </audio>
+          )
         }
+
+        {
+          changeTypeEdit === 2 &&
+          (
+            <OverlayTrigger
+              trigger="click"
+              placement="left"
+              overlay={popoverClick}
+            >
+              <div className="px-2">
+                {
+                  !data.url ?
+                    (
+                      <audio controls>
+                        Su navegador no es compatible con el elemento de audio.
+                      </audio>
+                    ) : (
+                      <audio controls>
+                        <source src={data.url} type="audio/ogg" />
+                        <source src={data.url} type="audio/mpeg" />
+                        <source src={data.url} type="audio/mp3" />
+                        Su navegador no es compatible con el elemento de audio.
+                      </audio>
+                    )
+                }
+              </div>
+              
+            </OverlayTrigger>
+          )
+        }
+
       </div>
     </div>
   )
