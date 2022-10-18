0f399002228a4e9c3246f25bf123658ee8eef1ea
Abdi Palma <abdi.palma@innappgt.com>
2022-10-18 14:03:10 -0600

    standby alert

--- src/app/modules/sitios/edit-site.tsx
+++ src/app/modules/sitios/edit-site.tsx
@@ -46,9 +46,12 @@ import {
 } from '../validarCadena/validadorCadena'
 import {KTSVG} from '../../../_metronic/helpers'
 import {Auth} from 'aws-amplify'
-import {roleManager} from '../../models/roleManager'
+import {roleManager} from '../../models/roleManager' 
+import { useContext } from 'react' 
+import { ContentContext } from '../template/movil/context' 
+import { LoadingContext } from '../../utility/component/loading/context'
 
-const customStyles = {
+const customStyles = { 
     control: (base: any, state: any) => ({
         ...base,
         background: '#1b1b29',
@@ -100,7 +103,9 @@ const customStyles = {
 }
 const animatedComponents = makeAnimated()
 
-const EditSite = () => {
+const EditSite = () => { 
+    //const { toogleSave, discardChange } = useContext(ContentContext) 
+    const { setShowLoad } = useContext(LoadingContext)
     const{id}=useParams()
     const {state} = useLocation()
     const [site, setSite] = useState<Site>({
@@ -236,7 +241,8 @@ const EditSite = () => {
     async function postDefault(route: string, object: any) {
         const sit: any = await postData(route, object)
     }
-    const changeStatus = async (favorito: boolean, publicado: boolean, oculto: boolean,cercania:boolean) => {
+    const changeStatus = async (favorito: boolean, publicado: boolean, oculto: boolean,cercania:boolean) => { 
+        setShowLoad(true)
        const respuesta3:any= await postData(statesMethod, {
             id_sitio: site.id_sitio,
             favorito: favorito,
@@ -284,7 +290,8 @@ const EditSite = () => {
         const getSites = async () => {
             const site: any = await getData(sitesMethod)
             // console.log(site)
-        }
+        } 
+        setShowLoad(false)
     }
 
     //alert methods-----------------------------------------------------------------------
@@ -603,8 +610,37 @@ const EditSite = () => {
                                     style={{color: '#92929F', display: 'flex', marginRight: '4px'}}
                                 ></Button>
 
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
                                 <Button
-                                    onClick={() => {
+                                    onClick={() => { 
+                                        //toogleSave()
                                         // status.publicado == false
                                         //   ? changeStatus(status.favorito, true, status.oculto)
                                         //   : changeStatus(status.favorito, false, status.oculto)
