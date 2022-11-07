60fc397d8148ce2cfcaeac6cf70e8f78a9b94ab0
Osmar Ortiz <osmar.ortiz@innappgt.com>
2022-11-07 09:04:18 -0600

    🔥 Name in QR download

--- src/app/modules/sitios/components/sitios-interes/edit-point-interes.tsx
+++ src/app/modules/sitios/components/sitios-interes/edit-point-interes.tsx
@@ -380,7 +380,7 @@ const EditPoint = () => {
         const pngUrl = canvas!.toDataURL('image/png').replace('image/png', 'image/octet-stream')
         let downloadLink = document.createElement('a')
         downloadLink.href = pngUrl
-        downloadLink.download = 'qr.png'
+        downloadLink.download = `${sitio.nombre}.png`
         document.body.appendChild(downloadLink)
         downloadLink.click()
         document.body.removeChild(downloadLink)
