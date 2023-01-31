11708ec3c4f7c14a0674318508826bc8422cc77a
Abdi Palma <abdi.palma@innappgt.com>
2023-01-31 10:08:29 -0600

    View password in restore password

--- src/app/modules/auth/components/RestorePassword.tsx
+++ src/app/modules/auth/components/RestorePassword.tsx
@@ -1,18 +1,30 @@
+import Visibility from '@mui/icons-material/Visibility'
+import VisibilityOff from '@mui/icons-material/VisibilityOff'
+import {FormControl, IconButton, InputAdornment, OutlinedInput} from '@mui/material'
 import {Auth} from 'aws-amplify'
 import {useState} from 'react'
 import {Form, Button} from 'react-bootstrap'
 import {Link, useNavigate} from 'react-router-dom'
 import swal from 'sweetalert'
+import {validateStringPassword} from '../../validarCadena/validadorCadena'
+
+interface State {
+    amount: string
+    password: string
+    weight: string
+    weightRange: string
+    showPassword: boolean
+}
 
 export function RestorePassword() {
     const [data, setData] = useState({password: '', passwordConfirm: '', code: ''})
     let navigate = useNavigate()
 
-    let email = localStorage.getItem('email') 
+    let email = localStorage.getItem('email')
 
-    if (email){ 
+    if (email) {
         email = JSON.parse(email)
-    } 
+    }
 
     const forgotPasswordSubmit = async () => {
         if (data.password != '' && data.passwordConfirm != '' && data.code != '') {
@@ -33,6 +45,37 @@ export function RestorePassword() {
             swal('Campos inválidos', 'Por favor ingresa correctamente los campos', 'warning')
         }
     }
+
+    const [password, setPassword] = useState('')
+    const [values, setValues] = useState({
+        amount: '',
+        password: '',
+        weight: '',
+        weightRange: '',
+        showPassword: false,
+    })
+    const [validPassword, setValidPassword] = useState(false)
+    const [touchedPasswordInput, setTouchedPasswordInput] = useState(false)
+    const handleChange = (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValues({...values, [prop]: event.target.value})
+        setTouchedPasswordInput(true)
+
+        if (validateStringPassword(event.target.value)) {
+            setPassword(event.target.value)
+        }
+
+        setValidPassword(validateStringPassword(event.target.value))
+    }
+
+    const handleClickShowPassword = () => {
+        setValues({
+            ...values,
+            showPassword: !values.showPassword,
+        })
+    }
+
+    const [validRepeatPassword, setValidRepeatPassword] = useState(false)
+    const [touchedRepeatPasswordInput, setTouchedRepeatPasswordInput] = useState(false)
 
     return (
         <div style={{width: '50%'}}>
@@ -64,7 +107,51 @@ export function RestorePassword() {
 
             <Form.Group className='mb-3'>
                 <Form.Label>Nueva contraseña</Form.Label>
-                <Form.Control
+                <p>
+                    *Debe tener por lo menos 6 caracteres, con letras mayúsculas, letras minúsculas,
+                    números y almenos un caracter especial*
+                </p>
+                <FormControl sx={{width: '100%'}} variant='outlined' color='primary' focused>
+                    <OutlinedInput
+                        inputProps={{
+                            style: {fontFamily: 'sans-serif', color: '#92929F'},
+                        }}
+                        id='outlined-adornment-password'
+                        type={values.showPassword ? 'text' : 'password'}
+                        onChange={(e) => {
+                            setTouchedPasswordInput(true)
+
+                            setData({
+                                password: e.target.value,
+                                passwordConfirm: data.passwordConfirm,
+                                code: data.code,
+                            })
+
+                            setValidPassword(validateStringPassword(e.target.value))
+
+                            handleChange('password')
+                        }}
+                        placeholder='Introduce tu contraseña'
+                        endAdornment={
+                            <InputAdornment position='end'>
+                                <IconButton
+                                    style={{color: '#92929F'}}
+                                    aria-label='toggle password visibility'
+                                    onClick={handleClickShowPassword}
+                                    edge='end'
+                                >
+                                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                    />
+                </FormControl>
+                {validPassword && touchedPasswordInput ? (
+                    <Form.Label className='text-primary'>Contraseña válida</Form.Label>
+                ) : !validPassword && touchedPasswordInput ? (
+                    <Form.Label className='text-danger'>Contraseña no válida</Form.Label>
+                ) : null}
+                {/* <Form.Control
                     type='password'
                     placeholder='Introduce tu contraseña'
                     autoComplete='off'
@@ -75,12 +162,50 @@ export function RestorePassword() {
                             code: data.code,
                         })
                     }}
-                />
+                /> */}
             </Form.Group>
 
             <Form.Group className='mb-3'>
                 <Form.Label>Confirma tu contraseña</Form.Label>
-                <Form.Control
+                <FormControl sx={{width: '100%'}} variant='outlined' color='primary' focused>
+                    <OutlinedInput
+                        inputProps={{style: {fontFamily: 'sans-serif', color: '#92929F'}}}
+                        id='outlined-adornment-password'
+                        type={values.showPassword ? 'text' : 'password'}
+                        onChange={(e) => {
+                            setTouchedRepeatPasswordInput(true)
+
+                            setValidRepeatPassword(validateStringPassword(e.target.value))
+
+                            setData({
+                                password: data.password,
+                                passwordConfirm: e.target.value,
+                                code: data.code,
+                            })
+
+                            handleChange('password')
+                        }}
+                        placeholder='Introduce tu contraseña'
+                        endAdornment={
+                            <InputAdornment position='end'>
+                                <IconButton
+                                    style={{color: '#92929F'}}
+                                    aria-label='toggle password visibility'
+                                    onClick={handleClickShowPassword}
+                                    edge='end'
+                                >
+                                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                    />
+                </FormControl>
+                {data.password == data.passwordConfirm ? (
+                    <Form.Label className='text-primary'>Las contraseñas coinciden</Form.Label>
+                ) : data.password != data.passwordConfirm ? (
+                    <Form.Label className='text-danger'>Las contraseñas no coinciden</Form.Label>
+                ) : null}
+                {/* <Form.Control
                     type='password'
                     placeholder='Repite tu contraseña'
                     autoComplete='off'
@@ -91,7 +216,7 @@ export function RestorePassword() {
                             code: data.code,
                         })
                     }}
-                />
+                /> */}
             </Form.Group>
 
             <Button
